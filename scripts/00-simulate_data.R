ecc91f0ad5da6b80a033be3819596f0355f9803e
Jenny Shen <jennycy.shen@mail.utoronto.ca>
2023-04-16 17:29:34 -0400

    updated simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,37 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Gather the relevant points, clean up the column names and refine their values
+# Author: Jenny Shen
+# Data: 04 April 2023
+# Contact: jennycy.shen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Access to the OECD employment rate and Permanent Immigrants Inflows to understand the recoding of values.
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(383)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+## Simulate a table with 
+## Focused on simulating a data set that covers United States, Canada, United Kingdom, Australia, and Japan
+## and the numbers of permanent inflows
+## and year (2008-2019)
+## and the in numbers of employed persons aged 15 to 64 as a percentage of working age population
+
+simulated_perm_employed_data <- tibble(
+  country = rep(c("United States", "Canada", "United Kingdom", "Australia", "Japan"), each = 12),
+  year = rep(2008:2019, times = 5),
+  employment_rate = c(
+    runif(n = 60, min = 50, max = 70) |> floor()
+  ),
+  permanent_inflows = c(
+    round(runif(60, 500000, 700000))
+  )
+)
+
+
+
+
+
 
 
 
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -12,10 +12,13 @@ library(haven)
 library(here)
 
 #### Clean data ####
-permanent_immigrant <- read_csv(here::here("inputs/data/DP_LIVE_04042023230723620.csv"))
-employment_rate <- read_csv(here::here("inputs/data/DP_LIVE_04042023230450558.csv"))
 
+### First, select variables corresponding to perminant immigrant, location, time, numbers of permanent inflows.
+permanent_immigrant <- read_csv(here::here("inputs/data/DP_LIVE_04042023230723620.csv"))
 permanent_immigrant_inflows_cleaned <- permanent_immigrant |> select(LOCATION,TIME, Value)
+
+### First, select variables corresponding to employment rate, location, time, in numbers of employed persons aged 15 to 64 as a percentage of working age population..
+employment_rate <- read_csv(here::here("inputs/data/DP_LIVE_04042023230450558.csv"))
 employment_rate_cleaned <- employment_rate |> select(LOCATION,TIME, Value)
 
 #### Save data ####
