bc4199289a021d2b7c75795516492722594c974f
Jenny Shen <jennycy.shen@mail.utoronto.ca>
2023-04-16 22:38:48 -0400

    Update test data

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -1,15 +1,30 @@
 #### Preamble ####
-# Purpose: Gather the relevant points, clean up the column names and refine their values
+# Purpose: Tests a dataset to ensure it's validity.
 # Author: Jenny Shen
 # Data: 04 April 2023
 # Contact: jennycy.shen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Access to the OECD employment rate and Permanent Immigrants Inflows to understand the recoding of values.
+# Pre-requisites: To have ran the cleaning script to test the cleaned datasets
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Test data ####
 
-## Test if Employment_Rate is less than 75
-merged_data$Employment_Rate |> min(na.rm = TRUE) > 75
+## Test if there are 5 countries
+merged_data$Country |> unique() |> length() >=5
+
+## Test if there are 11 years
+merged_data$Year |> min(na.rm = TRUE) >=11
+
+## Test if the lowest Employment Rate is 66.64754
+merged_data$Employment_Rate |> min() == 66.64754
+
+## Test if the highest Employment Rate is 77.16330
+merged_data$Employment_Rate |> max() == 77.16330
+
+## Test if the lowest permanent immigrant inflows is 55675.0
+merged_data$Permanent_Inflows |> min() == 55675.0
+
+## Test if the lowest permanent immigrant inflows is 1183505.0
+merged_data$Permanent_Inflows |> max() == 1183505.0
